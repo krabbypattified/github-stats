4669855bcc1801cda840eab1b3fd472510ed605e
Gabe Rogan <gabriel.rogan@uconn.edu>
2017-12-07 08:49:23 -0500

    fixed event path moz

--- src/helpers/listener.js
+++ src/helpers/listener.js
@@ -2,7 +2,9 @@ export let listener = {
 
   addNegative(target, type, callback) {
     document.addEventListener(type, event => {
-      if (!event.path.some(el => el === target)) callback(event)
+      let path = event.path || (event.composedPath && event.composedPath())
+      if (!path) return
+      if (!path.some(el => el === target)) callback(event)
     })
   },
 
